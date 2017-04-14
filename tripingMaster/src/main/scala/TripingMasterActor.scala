71b8d1f7cc94f11c2460342597d5ac9d0f34f277
Yuriy Netesov <ynetesov@cogniance.com>
2017-04-14 18:50:40 +0200

    triangulator code, not checked

--- tripingMaster/src/main/scala/TripingMasterActor.scala
+++ tripingMaster/src/main/scala/TripingMasterActor.scala
@@ -17,6 +17,17 @@ import cats.implicits._
 
 class TooFewSlavesError() extends RuntimeException("too few slaves")
 
+case class DoubleWithExp(d:Double) {
+  def **(x:Double):Double = Math.pow(d, x)
+}
+
+object DoubleWithExp {
+  implicit def doubleWithExp(x: Double): DoubleWithExp = new DoubleWithExp(x)
+}
+
+
+
+
 class TripingMasterActor extends Actor {
   private val log = Logging(context.system, this)
   var slaves:Map[ActorRef, String] = Map.empty
@@ -70,10 +81,24 @@ class TripingMasterActor extends Actor {
    }
 
   def triangulate(leftPings:List[Double], rightPings:List[Double], slavesPings:List[Double]):Double = {
+    import DoubleWithExp._
     val List(ax, bx, cx) = leftPings
     val List(ay, by, cy) = rightPings
     val List(ab, bc, ca) = slavesPings
-    ca
+
+    val cosBAC = (ab**2 + ca**2 - bc**2) / (2 * ab * ca)
+    val BAC = Math.acos(cosBAC)
+    val cosBAX = (ab**2 + ax**2 - bx**2) / (2 * ab * ax)
+    val BAX = Math.acos(cosBAX)
+    val cosCAY = (ca**2 + ay**2 - cy**2) / (2 * ca * ay)
+    val CAY = Math.acos(cosCAY)
+    val cosXAC = (ax**2 + ca**2 - cx**2) / (2 * ax * ca)
+    val XAC = Math.acos(cosXAC)
+
+    val XAY = BAC - CAY + (if (BAC < XAC) XAC else (-XAC))
+    val cosXAY = Math.cos(XAY)
+    val xy = Math.sqrt(ax**2 + ay**2 - 2 * ax * ay * cosXAY)
+    xy
   }
 }
 
